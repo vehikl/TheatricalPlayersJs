2f517b63c602d75b132793865d8e90b7f593a5f4
Nick Down <nick@nickdown.com>
2020-04-25 09:47:56 -0400

    extract method

--- src/statement.js
+++ src/statement.js
@@ -11,6 +11,10 @@ function statement (invoice, plays) {
         volumeCredits += Math.max(perf.audience - 30, 0);
     }
 
+    function addExtraCreditForComedy(play, perf) {
+        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    }
+
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
         let thisAmount = 0;
@@ -32,8 +36,7 @@ function statement (invoice, plays) {
                 throw new Error(`unknown type: ${play.type}`);
         }
         addVolumeCredits(perf);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        addExtraCreditForComedy(play, perf);
         // print line for this order
         result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
