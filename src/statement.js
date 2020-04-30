82e03dac6d6e2816e0fd5c4098838c8b23da674c
Colin DeCarlo <c.decarlo@vehikl.com>
2020-04-30 16:13:42 -0400

    format code

--- src/statement.js
+++ src/statement.js
@@ -1,43 +1,49 @@
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+  const format = new Intl.NumberFormat("en-US",
+    {
+      style: "currency", currency: "USD",
+      minimumFractionDigits: 2
+    }).format;
 
-function statement (invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        { style: "currency", currency: "USD",
-            minimumFractionDigits: 2 }).format;
+  function addVolumeCredits(performance) {
+    volumeCredits += Math.max(performance.audience - 30, 0);
+  }
 
-    for (let performance of invoice.performances) {
-        const play = plays[performance.playID];
-        let thisAmount = 0;
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (performance.audience > 30) {
-                    thisAmount += 1000 * (performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (performance.audience > 20) {
-                    thisAmount += 10000 + 500 * (performance.audience - 20);
-                }
-                thisAmount += 300 * performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
+  for (let performance of invoice.performances) {
+    const play = plays[performance.playID];
+    let thisAmount = 0;
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (performance.audience > 30) {
+          thisAmount += 1000 * (performance.audience - 30);
         }
-        // add volume credits
-        volumeCredits += Math.max(performance.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(performance.audience / 5);
-        // print line for this order
-        result += ` ${play.name}: ${format(thisAmount/100)} (${performance.audience} seats)\n`;
-        totalAmount += thisAmount;
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (performance.audience > 20) {
+          thisAmount += 10000 + 500 * (performance.audience - 20);
+        }
+        thisAmount += 300 * performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
-    return result;
+    // add volume credits
+    addVolumeCredits(performance);
+
+    // add extra credit for every ten comedy attendees
+    if ("comedy" === play.type) volumeCredits += Math.floor(performance.audience / 5);
+    // print line for this order
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${performance.audience} seats)\n`;
+    totalAmount += thisAmount;
+  }
+  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  return result;
 }
 
 module.exports = statement;
