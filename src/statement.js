86a7cf0b12ab8b5c25c2521689edf43ab614fde5
Colin DeCarlo <c.decarlo@vehikl.com>
2020-04-30 16:12:04 -0400

    rename variable

--- src/statement.js
+++ src/statement.js
@@ -7,32 +7,32 @@ function statement (invoice, plays) {
         { style: "currency", currency: "USD",
             minimumFractionDigits: 2 }).format;
 
-    for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
+    for (let performance of invoice.performances) {
+        const play = plays[performance.playID];
         let thisAmount = 0;
         switch (play.type) {
             case "tragedy":
                 thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
+                if (performance.audience > 30) {
+                    thisAmount += 1000 * (performance.audience - 30);
                 }
                 break;
             case "comedy":
                 thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                if (performance.audience > 20) {
+                    thisAmount += 10000 + 500 * (performance.audience - 20);
                 }
-                thisAmount += 300 * perf.audience;
+                thisAmount += 300 * performance.audience;
                 break;
             default:
                 throw new Error(`unknown type: ${play.type}`);
         }
         // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
+        volumeCredits += Math.max(performance.audience - 30, 0);
         // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === play.type) volumeCredits += Math.floor(performance.audience / 5);
         // print line for this order
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        result += ` ${play.name}: ${format(thisAmount/100)} (${performance.audience} seats)\n`;
         totalAmount += thisAmount;
     }
     result += `Amount owed is ${format(totalAmount/100)}\n`;
